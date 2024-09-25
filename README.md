fcc41b48577c4bf109a7d5e855f29088aa66e6ff
OluwaTobba <142856021+OluwaTobba@users.noreply.github.com>
2024-09-25 11:51:46 +0100

    Navbar Completed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is a fully functional hotel website, features to be added upon continuous development
+
+Features includes:
+1. Pop-up Modal for Login and Registration embedded in Navbar.
--- index.php
+++ index.php
@@ -2,10 +2,207 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hotel Management Website</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+
+    <title>MTC Hotel</title>
+
+    <!-- Bootstrap Styling -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!-- Local Styling -->
+    <style>
+
+        * {
+            font-family: "Poppins", sans-serif;
+        }
+
+        .h-font {
+            font-family: "Merienda", cursive;
+        }
+
+        /* number input hide */
+            /* Chrome, Safari, Edge, Opera */
+            input::-webkit-outer-spin-button,
+            input::-webkit-inner-spin-button {
+                -webkit-appearance: none;
+                margin: 0;
+            }
+
+            /* Firefox */
+            input[type=number] {
+                -moz-appearance: textfield;
+            }
+        /* end of number input hide */
+
+    </style>
 </head>
 <body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg nav-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
+
+        <div class="container-fluid">
+
+            <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="/index.php">MTC Hotel</a>
+
+            <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-2" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-2" href="#">Rooms</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-2" href="#">Facilities</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+
+                <div class="d-flex" role="search">
+                    <button type="button" class="btn btn-outline-dark me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
+                        Login
+                    </button>
+                    <button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#registerModal">
+                        Register
+                    </button>
+                </div>
+
+            </div>
+
+        </div>
+
+    </nav>
+    <!-- End of Navbar -->
     
+    <!-- Login Modal -->
+    <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+
+        <div class="modal-dialog">
+            
+            <div class="modal-content">
+                <form action="">
+
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5 d-flex align-items-center" id="staticBackdropLabel">
+                            <i class="bi bi-person-circle fs-3 me-2"></i> User Login
+                        </h1>
+                        <button type="reset" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label class="form-label">Email address</label>
+                            <input type="email" class="form-control shadow-none">
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label">Password</label>
+                            <input type="password" class="form-control shadow-none">
+                        </div>
+                        <div class="d-flex align-items-center justify-content-between mb-2">
+                            <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
+                            <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
+                        </div>
+                    </div>
+                
+                </form>
+            </div>
+
+        </div>
+
+    </div>
+    <!-- End of Login Modal -->
+
+    <!-- Registration Modal -->
+    <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+
+        <div class="modal-dialog modal-lg">
+            
+            <div class="modal-content">
+                <form action="">
+
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5 d-flex align-items-center" id="staticBackdropLabel">
+                            <i class="bi bi-person-lines-fill fs-3 me-2"></i> New User Registration 
+                        </h1>
+                        <button type="reset" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+
+                    <div class="modal-body">
+
+                        <span class="badge text-bg-light mb-3 text-wrap lh-base">
+                            Note: Your Details Must Match Your ID (NIN, National ID Card, Passport or Driving License). As it will be required during check-in. 
+                        </span>
+
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-6 ps-0 mb-3">
+                                    <label class="form-label">Name</label>
+                                    <input type="text" class="form-control shadow-none">
+                                </div>
+                                <div class="col-md-6 p-0 mb-3">
+                                    <label class="form-label">Email</label>
+                                    <input type="email" class="form-control shadow-none">
+                                </div>
+                                <div class="col-md-6 ps-0 mb-3">
+                                    <label class="form-label">Phone Number</label>
+                                    <input type="number" class="form-control shadow-none">
+                                </div>
+                                <div class="col-md-6 p-0 mb-3">
+                                    <label class="form-label">Picture</label>
+                                    <input type="file" class="form-control shadow-none">
+                                </div>
+                                <div class="col-md-12 ps-0 mb-3">
+                                    <label class="form-label">Address</label>
+                                    <textarea class="form-control shadow-none" rows="2"></textarea>
+                                </div>
+                                <div class="col-md-6 ps-0 mb-3">
+                                    <label class="form-label">Postal Code</label>
+                                    <input type="number" class="form-control shadow-none">
+                                </div>
+                                <div class="col-md-6 p-0 mb-3">
+                                    <label class="form-label">Date of Birth</label>
+                                    <input type="date" class="form-control shadow-none">
+                                </div>
+                                <div class="col-md-6 ps-0 mb-3">
+                                    <label class="form-label">Password</label>
+                                    <input type="password" class="form-control shadow-none">
+                                </div>
+                                <div class="col-md-6 p-0 mb-3">
+                                    <label class="form-label">Confirm Password</label>
+                                    <input type="password" class="form-control shadow-none">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="text-center my-1">
+                            <button type="submit" class="btn btn-dark shadow-none">REGISTER</button>
+                        </div>
+
+                    </div>
+                
+                </form>
+            </div>
+
+        </div>
+
+    </div>
+    <!-- End of Registration Modal -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
